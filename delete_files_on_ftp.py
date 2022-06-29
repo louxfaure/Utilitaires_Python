c69c4eff76172383df376c5a731205884b9756ba
louxfaure <louxfaure@gmail.com>
2022-06-29 13:56:47 +0200

    Gestion de plusieurs répertoires

--- delete_files_on_ftp.py
+++ delete_files_on_ftp.py
@@ -12,19 +12,20 @@ cnopts.hostkeys = None
 SERVICE = "Alma_Chargeur_De_Cours"
 LOGS_LEVEL = 'DEBUG'
 LOGS_DIR = os.getenv('LOGS_PATH')
+REP_A_NETTOYER = ['/DEPOT/THESES','/DEPOT/LEGANTO/ARCHIVES']
 
 
 
 with pysftp.Connection(host=os.getenv("SFTP_UB_HOSTNAME"), username=os.getenv("SFTP_UB_LOGIN"), password=os.getenv("SFTP_UB_PW"), cnopts=cnopts) as sftp:
     print("Connection succesfully stablished ... ")
-    remoteFilePath = '/DEPOT/THESES'
-    file_list = sftp.listdir(remoteFilePath)
-    print(file_list)
-    for entry in sftp.listdir_attr(remoteFilePath):
-        timestamp = entry.st_mtime
-        createtime = datetime.fromtimestamp(timestamp)
-        now = datetime.now()
-        delta = now - createtime
-        if delta.days > 15:
-            filepath = remoteFilePath + '/' + entry.filename
-            sftp.remove(filepath)
+    for remoteFilePath in REP_A_NETTOYER :
+        file_list = sftp.listdir(remoteFilePath)
+        print(file_list)
+        for entry in sftp.listdir_attr(remoteFilePath):
+            timestamp = entry.st_mtime
+            createtime = datetime.fromtimestamp(timestamp)
+            now = datetime.now()
+            delta = now - createtime
+            if delta.days > 15:
+                filepath = remoteFilePath + '/' + entry.filename
+                sftp.remove(filepath)
